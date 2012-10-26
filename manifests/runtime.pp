6fc657c40e5d89712c43bc1e11bfbaf8f7fb108d
Keith Burdis <keith@burdis.org>
2012-10-26 11:49:37 +0100

    Make base home directory configurable - will be useful for NFS-mounted home dirs.  Open ports when we have a runtime user.

--- manifests/runtime.pp
+++ manifests/runtime.pp
@@ -1,7 +1,8 @@
 define wso2esb::runtime  (
   $version = undef,
   $user    = undef,
-  $group   = undef
+  $group   = undef,
+  $home    = '/home'
 ) {
   if $version == undef {
     fail('wso2esb::runtime version parameter required')
@@ -12,54 +13,54 @@ define wso2esb::runtime  (
   if $group == undef {
     fail('wso2esb::runtime group parameter required')
   }
-  file { "/home/${user}/service/wso2esb":
+  file { "${home}/${user}/service/wso2esb":
     ensure  => directory,
     mode    => '0755',
     owner   => $user,
     group   => $group,
   }
-  file { "/home/${user}/service/wso2esb/run":
+  file { "${home}/${user}/service/wso2esb/run":
     ensure  => present,
     mode    => '0555',
     owner   => $user,
     group   => $group,
     content => template('wso2esb/run.erb'),
-    require => File["/home/${user}/service/wso2esb"],
+    require => File["${home}/${user}/service/wso2esb"],
   }
-  file { "/home/${user}/service/wso2esb/log":
+  file { "${home}/${user}/service/wso2esb/log":
     ensure  => directory,
     mode    => '0755',
     owner   => $user,
     group   => $group,
-    require => File["/home/${user}/service/wso2esb"],
+    require => File["${home}/${user}/service/wso2esb"],
   }
-  file { "/home/${user}/service/wso2esb/log/run":
+  file { "${home}/${user}/service/wso2esb/log/run":
     ensure  => present,
     mode    => '0555',
     owner   => $user,
     group   => $group,
     content => template('wso2esb/log_run.erb'),
-    require => File["/home/${user}/service/wso2esb/log"],
+    require => File["${home}/${user}/service/wso2esb/log"],
   }
 
   $subdir  = "wso2esb-${version}"
   exec { 'wso2esb-copy-product':
-    cwd     => "/home/${user}",
-    command => "/usr/bin/rsync -a '/opt/wso2/${subdir}' /home/${user}",
+    cwd     => "${home}/${user}",
+    command => "/usr/bin/rsync -a '/opt/wso2/${subdir}' ${home}/${user}",
     require => Class['wso2esb::install'],
-    creates => "/home/${user}/${subdir}",
+    creates => "${home}/${user}/${subdir}",
     user    => $user,
     group   => $group,
     notify  => Exec['wso2esb-disable-scripts'],
   }
   exec { 'wso2esb-disable-scripts':
-    cwd         => "/home/${user}/${subdir}/bin",
+    cwd         => "${home}/${user}/${subdir}/bin",
     command     => "/bin/sed -e '1aecho Run:' -e '1aecho \"  sv start|stop|status wso2esb\"' -e '1aecho instead' -e '1aexit 1' -i wso2server.sh wso2esb-samples.sh",
     user        => $user,
     group       => $group,
     refreshonly => true,
   }
-  file { "/home/${user}/wso2esb":
+  file { "${home}/${user}/wso2esb":
     ensure  => link,
     target  => $subdir,
     owner   => $user,
@@ -69,4 +70,16 @@ define wso2esb::runtime  (
   # TODO: Add required config files from templates
   # TODO: Add JDBC database drivers -> $basedir/esb/lib/endorsed
   runit::user{ $user: group => $group }
+  iptables::allow{ "wso2esb-http_wsdl-${user}":
+    port     => '8280',
+    protocol => 'tcp'
+  }
+  iptables::allow{ "wso2esb-https_wsdl-${user}":
+    port     => '8243',
+    protocol => 'tcp',
+  }
+  iptables::allow{ "wso2esb-console-${user}":
+    port     => '9443',
+    protocol => 'tcp'
+  }
 }
