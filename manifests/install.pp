1f9a92092edc8c239c23a1c049f25d33e6c21f96
Keith Burdis <keith@burdis.org>
2012-11-18 14:36:21 +0000

    Avoid resource conflicts.

--- manifests/install.pp
+++ manifests/install.pp
@@ -6,7 +6,7 @@ define wso2::install (
 ) {
   $version = $name
   $zipfile = "${version}.zip"
-  $subdir  = "${version}"
+  $subdir  = $version
   if ! defined(Package['unzip']) {
     package { 'unzip': ensure => installed }
   }
@@ -21,35 +21,35 @@ define wso2::install (
     owner => $user,
     group => $group,
   }
-  file { 'wso2-zipfile':
+  file { "wso2-zipfile-${version}":
     ensure  => present,
     path    => "/root/wso2/${zipfile}",
     mode    => '0444',
     source  => "puppet:///files/${zipfile}",
     require => File['/root/wso2'],
   }
-  exec { 'wso2-unpack':
+  exec { "wso2-unpack-${version}":
     cwd     => $basedir,
     command => "/usr/bin/unzip '/root/wso2/${zipfile}'",
     creates => "${basedir}/${subdir}",
-    notify  => Exec['wso2-fix-ownership'],
-    require => [ Exec['wso2-basedir'], File['wso2-zipfile'] ],
+    notify  => Exec["wso2-fix-ownership-${version}"],
+    require => [ Exec['wso2-basedir'], File["wso2-zipfile-${version}"] ],
   }
   file { "${basedir}/${subdir}":
     ensure  => directory,
-    require => Exec['wso2-unpack']
+    require => Exec["wso2-unpack-${version}"],
   }
-  exec { 'wso2-fix-ownership':
+  exec { "wso2-fix-ownership-${version}":
     command     => "/bin/chown -R ${user}:${group} ${basedir}/${subdir}",
     refreshonly => true,
   }
-  if $default {
-    file { "${basedir}/wso2":
-      ensure  => link,
-      target  => "${basedir}/${subdir}",
-      require => Exec['wso2-basedir'],
-    }
-  }
+  #TODO if $default {
+  #TODO   file { "${basedir}/wso2":
+  #TODO     ensure  => link,
+  #TODO     target  => "${basedir}/${subdir}",
+  #TODO     require => Exec['wso2-basedir'],
+  #TODO   }
+  #TODO }
   # TODO: Add required config files from templates
   # TODO: Add JDBC database drivers -> ${wso2::basedir}/esb/lib/endorsed
 }
