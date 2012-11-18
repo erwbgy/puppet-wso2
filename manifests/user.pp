74b44d14fa44f25e7936a4cfae4d04aede909ff7
Keith Burdis <keith@burdis.org>
2012-11-18 14:36:32 +0000

    User runit::service type

--- manifests/user.pp
+++ manifests/user.pp
@@ -1,46 +1,23 @@
 define wso2::user  (
-  $version = undef,
+  $product = undef,
   $group   = $::wso2::group,
   $home    = '/home'
 ) {
-  $user = $name
-  if $version == undef {
-    fail('wso2::runtime version parameter required')
+  $user = $title
+  runit::service { $product:
+    user        => $user,
+    group       => $group,
   }
-  if $user == undef {
-    fail('wso2::runtime user parameter required')
-  }
-  file { "${home}/${user}/service/wso2":
-    ensure  => directory,
-    mode    => '0755',
-    owner   => $user,
-    group   => $group,
-  }
-  file { "${home}/${user}/service/wso2/run":
-    ensure  => present,
-    mode    => '0555',
-    owner   => $user,
-    group   => $group,
-    content => template('wso2/run.erb'),
-    require => File["${home}/${user}/service/wso2"],
-  }
-  file { "${home}/${user}/service/wso2/log":
-    ensure  => directory,
-    mode    => '0755',
-    owner   => $user,
-    group   => $group,
-    require => File["${home}/${user}/service/wso2"],
-  }
-  file { "${home}/${user}/service/wso2/log/run":
-    ensure  => present,
-    mode    => '0555',
-    owner   => $user,
-    group   => $group,
-    content => template('wso2/log_run.erb'),
-    require => File["${home}/${user}/service/wso2/log"],
-  }
+  ##file { "${home}/${user}/service/wso2/run":
+  ##  ensure  => present,
+  ##  mode    => '0555',
+  ##  owner   => $user,
+  ##  group   => $group,
+  ##  content => template('wso2/run.erb'),
+  ##  require => File["${home}/${user}/service/wso2"],
+  ##}
 
-  $subdir  = $version
+  $subdir  = $product
   exec { "wso2-copy-product-${user}":
     cwd     => "${home}/${user}",
     command => "/usr/bin/rsync -a '/opt/wso2/${subdir}' ${home}/${user}",
@@ -57,26 +34,13 @@ define wso2::user  (
     group       => $group,
     refreshonly => true,
   }
-  file { "${home}/${user}/wso2":
-    ensure  => link,
-    target  => $subdir,
-    owner   => $user,
-    group   => $group,
-    replace => false,
-  }
+  ##file { "${home}/${user}/wso2":
+  ##  ensure  => link,
+  ##  target  => $subdir,
+  ##  owner   => $user,
+  ##  group   => $group,
+  ##  replace => false,
+  ##}
   # TODO: Add required config files from templates
   # TODO: Add JDBC database drivers -> $basedir/esb/lib/endorsed
-  #runit::user{ $user: group => $group }
-  #iptables::allow{ "wso2-http_wsdl-${user}":
-  #  port     => '8280',
-  #  protocol => 'tcp'
-  #}
-  #iptables::allow{ "wso2-https_wsdl-${user}":
-  #  port     => '8243',
-  #  protocol => 'tcp',
-  #}
-  #iptables::allow{ "wso2-console-${user}":
-  #  port     => '9443',
-  #  protocol => 'tcp'
-  #}
 }
