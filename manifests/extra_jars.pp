d75b0551ac5f9879216557c7ed665c1e017da3a6
Keith Burdis <keith@burdis.org>
2012-11-29 11:54:41 +0000

    Assume that the group is wso2 for now

--- manifests/extra_jars.pp
+++ manifests/extra_jars.pp
@@ -1,7 +1,8 @@
 define wso2::extra_jars (
   $product_dir,
   $user,
-  $home = '/home',
+  $group = 'wso2',
+  $home  = '/home',
 ) {
   $jar_file = regsubst($title, "^${product_dir}/", '')
   file { "${product_dir}/repository/components/lib/${jar_file}":
