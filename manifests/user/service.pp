9d85c9ae319626becea67f6c6f1e4f79e2c123ec
Keith Burdis <keith@burdis.org>
2012-11-29 11:25:09 +0000

    Product installation takes place elsewhere.  This just creates the Runit service directory.

--- manifests/user/service.pp
+++ manifests/user/service.pp
@@ -1,4 +1,5 @@
 define wso2::user::service  (
+  $product   = undef,
   $user      = undef,
   $group     = undef,
   $version   = undef,
@@ -6,49 +7,30 @@ define wso2::user::service  (
   $java_opts = '',
   $home      = '/home'
 ) {
-  if $version {
-    $product = $title
-    $subdir  = "${product}-${version}"
-    exec { "wso2-copy-${product}-${user}":
-      cwd     => "${home}/${user}",
-      command => "/usr/bin/rsync -a '/opt/wso2/${subdir}' ${home}/${user}",
-      require => File["${::wso2::basedir}/${subdir}"],
-      creates => "${home}/${user}/${subdir}",
-      user    => $user,
-      group   => $group,
-    }
-    file { "${home}/${user}/${product}":
-      ensure  => link,
-      target  => $subdir,
-      owner   => $user,
-      group   => $group,
-      replace => false,
-      require => Exec["wso2-copy-${product}-${user}"],
-    }
-    runit::service { $product:
-      user        => $user,
-      group       => $group,
-    }
-    file { "${home}/${user}/runit/${product}/run":
-      ensure  => present,
-      mode    => '0555',
-      owner   => $user,
-      group   => $group,
-      content => template("wso2/${product}/run.erb"),
-      require => File["${home}/${user}/runit/${product}"],
-    }
-    file { "${home}/${user}/service/${product}":
-      ensure  => link,
-      target  => "${home}/${user}/runit/${product}",
-      owner   => $user,
-      group   => $group,
-      replace => false,
-      require => File["${home}/${user}/runit/${product}/run"],
-    }
-    file { "${home}/${user}/${product}/repository/logs":
-      ensure => link,
-      owner  => $user,
-      target => "${home}/${user}/logs/${product}",
-    }
+  runit::service { "${user}-${product}":
+    service     => $product,
+    user        => $user,
+    group       => $group,
+  }
+  file { "${home}/${user}/runit/${product}/run":
+    ensure  => present,
+    mode    => '0555',
+    owner   => $user,
+    group   => $group,
+    content => template("wso2/${product}/run.erb"),
+    require => File["${home}/${user}/runit/${product}"],
+  }
+  file { "${home}/${user}/service/${product}":
+    ensure  => link,
+    target  => "${home}/${user}/runit/${product}",
+    owner   => $user,
+    group   => $group,
+    replace => false,
+    require => File["${home}/${user}/runit/${product}/run"],
+  }
+  file { "${home}/${user}/${product}/repository/logs":
+    ensure => link,
+    owner  => $user,
+    target => "${home}/${user}/logs/${product}",
   }
 }
