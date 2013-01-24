5487d9027958890b2c3b0ebfe48a18eed88155a3
Keith Burdis <keith@burdis.org>
2013-01-24 13:29:09 +0000

    Comment out other products until they have been updated to be the same as ESB.

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,17 +8,17 @@ class wso2 {
     mode   => '0750',
   }
 
-  # API Manager
-  $am = hiera_hash('wso2::am', undef)
-  if $am {
-    create_resources('wso2::am', $am)
-  }
-
-  # Business Actvity Monitor
-  $bam = hiera_hash('wso2::bam', undef)
-  if $bam {
-    create_resources('wso2::bam', $bam)
-  }
+#  # API Manager
+#  $am = hiera_hash('wso2::am', undef)
+#  if $am {
+#    create_resources('wso2::am', $am)
+#  }
+#
+#  # Business Actvity Monitor
+#  $bam = hiera_hash('wso2::bam', undef)
+#  if $bam {
+#    create_resources('wso2::bam', $bam)
+#  }
 
   # Enterprise Service Bus
   $esb = hiera_hash('wso2::esb', undef)
@@ -26,15 +26,15 @@ class wso2 {
     create_resources('wso2::esb', $esb)
   }
 
-  # Governance Registry
-  $greg = hiera_hash('wso2::greg', undef)
-  if $greg {
-    create_resources('wso2::greg', $greg)
-  }
-
-  # Identity Server
-  $is = hiera_hash('wso2::is', undef)
-  if $is {
-    create_resources('wso2::is', $is)
-  }
+#  # Governance Registry
+#  $greg = hiera_hash('wso2::greg', undef)
+#  if $greg {
+#    create_resources('wso2::greg', $greg)
+#  }
+#
+#  # Identity Server
+#  $is = hiera_hash('wso2::is', undef)
+#  if $is {
+#    create_resources('wso2::is', $is)
+#  }
 }
